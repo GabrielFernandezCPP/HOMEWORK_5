4590d15a0717035f14796dfbb72d2903271588c0
Gabriel Fernandez <grfernandez@cpp.edu>
2025-03-11 17:57:55 -0700

    Purpose fail test

--- Calculator/test_calculator.c
+++ Calculator/test_calculator.c
@@ -10,11 +10,27 @@ void tearDown(void) {
 }
 
 void test_add_positive_numbers(void) {
-    TEST_ASSERT_EQUAL(6, add(2, 3)); //We expect this to be 5
+    TEST_ASSERT_EQUAL(5, add(2, 3)); //We expect this to be 5
+}
+
+void test_add_positive_and_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(1, add(5, -4)); //Expect to be 1
+}
+
+void test_add_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(-7, add(-3, -4)); //Expect to be -7
+}
+
+void test_add_zero(void) {
+    TEST_ASSERT_EQUAL(10, add(10, 0));
+    TEST_ASSERT_EQUAL(0, add(0, 0));
 }
 
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);
+    RUN_TEST(test_add_positive_and_negative_numbers);
+    RUN_TEST(test_add_negative_numbers);
+    RUN_TEST(test_add_zero);
     return UNITY_END();
 }
